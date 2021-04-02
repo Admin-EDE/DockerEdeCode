180a5ed670dba2ee4660c993a51e45acb55ad9ae
MAP-Mineduc <miguel.aedo@innovacion.mineduc.cl>
2021-04-01 21:47:58 -0300

    fusión de archivos parseCSVtoEDE.py y ede.py

--- parseCSVtoEDE.py
+++ parseCSVtoEDE.py
@@ -1,46 +1,88 @@
 # -*- coding: utf-8 -*-
-import requests, io, os, sys
+import logging
+logger = logging.getLogger('root')
 
-def downloadFile(idFile='1j-1QXj-vM-pTYxsemzjFaszs-Sgwf4iM',fileName='ede.py'):
-  urlFile = f'http://drive.google.com/uc?export=download&id={idFile}' 
-  print(f"Intentando descargar el archivo '{fileName}' desde '{urlFile}'...")
-  try:
-    response = requests.get(urlFile, stream=True)
-    response.raise_for_status()
-  except:
-    pathFile = None
-  else:
-    with open(fileName,'wb') as out:
-      out.write(io.BytesIO(response.content).read()) ## Read bytes into file
-    pathFile = os.path.join(os.path.dirname(fileName), fileName)
-  return pathFile
+#import argparse #Librería usada en funcion main() para crear el menú de la consola
+import importlib.util #utilizada en función module_from_file para cargar las librerías del estándar
+from zipfile import ZipFile
+import io, os, sys 
+from datetime import datetime
+from pytz import timezone 
+from time import time #importamos la función time para capturar tiempos
 
 def main():
-  print(f"Iniciando programa...")
-  fileName='ede.py'
-  #pathDownloadFile = downloadFile()
+  tiempo_inicial = time()
+  t_stamp = str(int(datetime.timestamp(datetime.now(timezone('Chile/Continental')))))
+  logger = setup_custom_logger('root', t_stamp)
+
+  logger.info("Iniciando ejecución desde 'parseCSVtoEDE.py'...")
+
   if(len(sys.argv) < 2):
     sys.argv.append('--help')
 
-  if('update' in sys.argv):
-    pathDownloadFile = downloadFile()
+  module = module_from_file("ede", "./ede/ede/consoleMenu.py")
+  menu = module.consoleMenu()
+  args = menu.parser.parse_args() #Obtiene la lista desde la consola
+
+  args._encode = 'utf8' #Opciones Windows:'cp1252', Google Colab: 'utf8'
+  args._sep = ';'       #Opciones Windows:';', Google Colab: ','
+  args.path_to_dir_csv_file = './csv/'
+  args.t_stamp = t_stamp
+
+  if('func' in args): 
+    args.func(args); #Ejecuta la función por defecto
   else:
-    pathDownloadFile = None
+    logger.info(parser.format_help())
+    
+  zipFile = ZipFile(f'./{t_stamp}_Data.zip','a')
+  listFiles = [
+    f'./{t_stamp}_key.txt',
+    f'./{t_stamp}_key.encrypted',
+    f'./{t_stamp}_ForenKeyErrors.csv',
+    f'./{t_stamp}_LOG.txt',
+    f'./{t_stamp}_encryptedD3.db'
+    ] + [os.path.join(r,file) for r,d,f in os.walk("./csv") for file in f] 
 
-  if (not pathDownloadFile):
-    print(f"No se descargó el archivo '{fileName}' desde Internet...\nSe buscará una copia local...")
-    if (os.path.exists(fileName)):
-      print(f"Se encontró una copia local del archivo '{fileName}'...")
-      pathDownloadFile = fileName
+  logger.info(f"Archivos a comprimir {listFiles}")
 
-  if(pathDownloadFile):
-    print(f"Llamando el archivo: {pathDownloadFile}")
-    import ede
-    ede.main()
-  else:
-    print(f"No se pudo descargar el archivo '{fileName}' y tampoco se encontró una copia local del archivo.")
-    print("Si lo desea puede utilizar el argumento --debug para obtener más detalles del error.\n")
-    print("Si no tiene acceso a Internet, descargue una copia manualmente del archivo en su carpeta local.")
+  for file in listFiles:
+    if os.path.exists(file):
+      if not file.endswith('_key.txt') and not file.startswith('./csv/'):
+        zipFile.write(file)
+      if file.startswith('./csv/') and "parse" in sys.argv:
+        zipFile.write(file)
+      if not file.startswith('./csv/'):
+        os.remove(file)
 
+  logger.info("Finalizando ejecución desde 'ede.py'...")
+  logger.info(f'Tiempo de ejecucion: {str(time() - tiempo_inicial)}')
+  
 if __name__== "__main__":
-  main()
+  main();  
+
+def module_from_file(module_name, file_path):
+    spec = importlib.util.spec_from_file_location(module_name, file_path)
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+    return module
+
+def setup_custom_logger(name, t_stamp):
+  _nameLogFile = f'./{t_stamp}_LOG.txt'
+  stream_formatter = logging.Formatter(fmt='%(asctime)s - %(levelname)s - %(module)s - %(levelname)s - %(message)s')
+  file_formatter=logging.Formatter(
+    '{"time": "%(asctime)s", "name": "%(name)s", "module": "%(module)s", "funcName": "%(funcName)s", \
+      "lineno": "%(lineno)d", "levelname": "%(levelname)s", "message": "%(message)s", "pathname": "%(pathname)s"}'
+  )
+
+  handlerStream = logging.StreamHandler(); 
+  handlerStream.setFormatter(stream_formatter)
+  
+  handlerFile = logging.FileHandler(_nameLogFile, mode='w', encoding='utf8')
+  handlerFile.setFormatter(file_formatter)
+
+  logger = logging.getLogger(name)
+  _logginLevel = logging.DEBUG if "--debug" in sys.argv else logging.INFO
+  logger.setLevel(_logginLevel)
+  logger.addHandler(handlerStream)
+  logger.addHandler(handlerFile)
+  return logger
