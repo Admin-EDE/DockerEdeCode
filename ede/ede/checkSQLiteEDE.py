27505c351b1b580c756204ef40dff1322a965467
Admin-EDE <admin@ede.mineduc.cl>
2021-11-23 12:18:19 -0300

    fix fn5E5 se filtra por Bloque02 y se comparan resultados

--- ede/ede/checkSQLiteEDE.py
+++ ede/ede/checkSQLiteEDE.py
@@ -3017,39 +3017,154 @@ class check:
   def fn5E5(self,conn):
         try:
             _query = conn.execute("""
-            SELECT
-                RAE.digitalRandomKey,
-                RAE.Date
-            FROM Organization O
-                    join OrganizationPersonRole OPR on O.OrganizationId = OPR.OrganizationId
-                    join RoleAttendanceEvent RAE on OPR.OrganizationPersonRoleId = RAE.OrganizationPersonRoleId
-            where O.RefOrganizationTypeId = 22
-            and OPR.RoleId in (4, 5)
-            and RAE.Date between(RAE.Date||'10:00:00') and (RAE.Date||'11:45:00');
-            """).fetchall()
-            _lenQuery = int(len(_query))
-            _contador = 0
+                  SELECT 
+                    count(rae_.refattendancestatusid) as 'totalEstudiantesCurso', -- Cantidad total de estudiantes [idx 0]
+                    sum(CASE WHEN rae_.refattendancestatusid IN (1) THEN 1 ELSE 0 END) as 'estudiantesPresentesCurso', -- [idx 1]
+                    sum(CASE WHEN rae_.refattendancestatusid IN (2,3) THEN 1 ELSE 0 END) as 'estudiantesAusentesv', -- [idx 2]
+                    sum(CASE WHEN rae_.refattendancestatusid IN (4) THEN 1 ELSE 0 END) as 'estudiantesRetrasadosCurso' -- [idx 3]
+                    ,a.*
+                  FROM Organization O
+                    JOIN RefOrganizationType rot
+                      ON O.RefOrganizationTypeId = rot.RefOrganizationTypeId
+                      AND O.RefOrganizationTypeId IN (
+                        SELECT RefOrganizationTypeId 
+                        FROM RefOrganizationType
+                        WHERE Description IN ('Course')
+                      ) 
+                    OUTER LEFT JOIN OrganizationPersonRole opr_
+                      ON O.OrganizationId = opr_.OrganizationId
+                      AND opr_.RecordEndDateTime IS NULL
+                    OUTER LEFT JOIN RoleAttendanceEvent rae_
+                      ON opr_.OrganizationPersonRoleId = rae_.OrganizationPersonRoleId
+                      AND rae_.RecordEndDateTime IS NULL
+                    OUTER LEFT JOIN
+                    (
+                    SELECT  
+                      rae.Date, -- fecha completa de la clase [idx 0]
+                      ors.Parent_OrganizationID,		
+                      O.OrganizationId,
+                      strftime('%Y-%m-%d', rae.Date) as 'fecha', -- rescata solo la fecha desde rae.Date [idx 1]
+                      CASE 
+                        WHEN strftime('%w', rae.Date) = '0' THEN 'Domingo'
+                        WHEN strftime('%w', rae.Date) = '1' THEN 'Lunes'
+                        WHEN strftime('%w', rae.Date) = '2' THEN 'Martes'
+                        WHEN strftime('%w', rae.Date) = '3' THEN 'Miercoles'
+                        WHEN strftime('%w', rae.Date) = '4' THEN 'Jueves'
+                        WHEN strftime('%w', rae.Date) = '5' THEN 'Viernes'
+                        WHEN strftime('%w', rae.Date) = '6' THEN 'Sabado'
+                      END as 'diaSemana', -- rescata solo el dpia de la semana desde rae.Date [idx 3]
+                      count(refattendancestatusid) as 'totalEstudiantes', -- Cantidad total de estudiantes [idx 4]
+                      sum(CASE WHEN refattendancestatusid IN (1) THEN 1 ELSE 0 END) as 'estudiantesPresentesAsignatura', -- [idx 5]
+                      sum(CASE WHEN refattendancestatusid IN (2,3) THEN 1 ELSE 0 END) as 'estudiantesAusentesAsignatura', -- [idx 6]
+                      sum(CASE WHEN refattendancestatusid IN (4) THEN 1 ELSE 0 END) as 'estudiantesRetrasadosAsignatura' -- [idx 7]
+                      
+                    FROM Organization O
+                      JOIN RefOrganizationType rot
+                        ON O.RefOrganizationTypeId = rot.RefOrganizationTypeId
+                        AND O.RefOrganizationTypeId IN (
+                          SELECT RefOrganizationTypeId 
+                          FROM RefOrganizationType
+                          WHERE Description IN ('Course Section')
+                        ) 
+                      JOIN OrganizationRelationship ors 
+                        ON O.OrganizationId = ors.OrganizationId
+                        AND ors.RefOrganizationRelationShipId IN (
+                          SELECT RefOrganizationRelationShipId 
+                          FROM RefOrganizationRelationShip
+                          WHERE Code IN ('InternalOrganization')
+                          ) 
+                      JOIN OrganizationPersonRole opr 
+                        ON O.OrganizationId = opr.OrganizationId
+                        AND opr.RecordEndDateTime IS NULL
+                      JOIN RoleAttendanceEvent rae
+                        ON opr.OrganizationPersonRoleId = rae.OrganizationPersonRoleId
+                        AND rae.RecordEndDateTime IS NULL
+                      JOIN Role rol
+                        ON opr.RoleId = rol.RoleId
+                        AND opr.RoleId IN (
+                          SELECT RoleId
+                          FROM Role
+                          WHERE Name IN ('Estudiante')
+                        )
+                      JOIN OrganizationCalendar oc 
+                        ON O.OrganizationId = oc.OrganizationId
+                        AND oc.RecordEndDateTime IS NULL
+                      JOIN OrganizationCalendarSession ocs
+                        ON oc.OrganizationCalendarId = ocs.OrganizationCalendarId
+                        AND ocs.RecordEndDateTime IS NULL
+                      JOIN CourseSectionSchedule css
+                        ON O.OrganizationId = css.OrganizationId
+                        AND css.RecordEndDateTime IS NULL
+                    --GROUP BY rae.Date
+
+                    WHERE 
+                      -- Verifica que se encuentre cargado el leccionario
+                      rae.RefAttendanceEventTypeId = 2
+                      AND
+                      -- Verifica que se encuentre cargado el leccionario
+                      ocs.Description NOT NULL
+                      AND
+                      -- Verifica que el indicador sea True, ya que en estos casos corresponde la relación	
+                      ocs.AttendanceTermIndicator IN (1)
+                      AND
+                      -- Verifica que la firma se encuentre cargada en el sistema
+                      rae.digitalRandomKey NOT NULL
+                      AND
+                      -- Verifica que se haya especificado si es estudiante asiste presencialmente o no.
+                      rae.VirtualIndicator NOT NULL
+                      AND
+                      -- Verifica que día y horario de firma corresponda con calendario de la asignatura
+                      diaSemana in (css.ClassMeetingDays)
+                      AND
+                      -- Agrega a la lista todos los registros que no cumplan con la expresión regular
+                      rae.Date REGEXP '^(19|2[0-9])[0-9]{2}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])T(0[0-9]|1[0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])((\\+|-)(0[0-9]|1[0-9]|2[0-3]):([0-5][0-9]))$'
+                      AND
+                      -- Agrega a la lista todos los registros que no cumplan con la expresión regular
+                      rae.digitalRandomKeyDate REGEXP '^(19|2[0-9])[0-9]{2}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])T(0[0-9]|1[0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])((\\+|-)(0[0-9]|1[0-9]|2[0-3]):([0-5][0-9]))$'
+                      AND
+                      -- Agrega a la lista todos los registros que no cumplan con la expresión regular
+                      rae.digitalRandomKey REGEXP '^[0-9]{6}+([-]{1}[0-9kK]{1})?$'
+                      AND
+                      ClassPeriod in ('Bloque02')
+                      
+                    GROUP BY rae.Date
+                    
+                    ) a ON O.OrganizationId = a.Parent_OrganizationID
+                  WHERE
+                    -- Verifica que el tipo de asistencia sea diaria
+                    rae_.RefAttendanceEventTypeId = 1
+            """)#.fetchall()
+            if(_query.returns_rows == 0):
+              logger.info(f"Rechazado")
+              logger.info(f"No hay información para evaluar")
+              return False
+            
+            _query = _query.fetchall()
             if(len(_query)>0):
-                _digitalRandomKey = (list([m[0] for m in _query if m[0] is not None]))
-                if not _digitalRandomKey:
-                    logger.error(f"Sin firmas registradas")
-                    logger.error(f'Rechazado')
-                    return False
-                for x in _digitalRandomKey:
-                    if x is None:
-                        logger.error(f'Sin firmas registradas')
+                _totalCurso = (list([m[0] for m in _query if m[0] is not None]))
+                _totalAsign = (list([m[9] for m in _query if m[9] is not None]))
+                
+                _estPresentesCurso = (list([m[1] for m in _query if m[1] is not None]))
+                _estPresentesAsign = (list([m[10] for m in _query if m[10] is not None]))
+                _estAtradadosAsign = (list([m[12] for m in _query if m[12] is not None]))                
+
+                _estAusentesCurso = (list([m[2] for m in _query if m[2] is not None]))
+                _estAusentesAsign = (list([m[11] for m in _query if m[11] is not None]))
+                
+                for idx_,el_ in enumerate(_totalCurso):
+                    if el_ != _totalAsign[idx_]:
                         logger.error(f'Rechazado')
-                        return False
-                    if (len(x)>0):
-                        _contador += 1
-                if(_lenQuery == _contador):
-                    logger.info(f'Firmas registradas correctamente')
-                    logger.info(f'Aprobado')
-                    return True
-            else:
-                logger.error(f'S/Datos')
-                logger.error(f'Sin datos de asistencia por bloque')
-                return False
+                        logger.error(f'Totales de estudiantes no coinciden')                        
+
+                    if _estPresentesCurso[idx_] != (_estPresentesAsign[idx_]+_estAtradadosAsign[idx_]):
+                        logger.error(f'Rechazado')
+                        logger.error(f'Total de estudiantes presentes no coinciden')
+
+                    if _estAusentesCurso[idx_] != _estAusentesAsign[idx_]:
+                        logger.error(f'Rechazado')
+                        logger.error(f'Total de estudiantes ausentes no coinciden')
+
+                    return False                      
         except Exception as e:
             logger.error(f"No se pudo ejecutar la consulta: {str(e)}")
             logger.error(f"Rechazado")
