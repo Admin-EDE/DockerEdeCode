5a1fc9ceba048bd69e93672bdc614cf06ac1bc84
Admin-EDE <admin@ede.mineduc.cl>
2021-12-09 12:07:53 -0300

    fix fn5E0 se modifica _ExistData if

--- ede/ede/checkSQLiteEDE.py
+++ ede/ede/checkSQLiteEDE.py
@@ -32,7 +32,7 @@ def sqlite_regexp(expr, item):
     reg = re.compile(expr)
     #logger.info(f"reg:{reg}")
     try:
-        logger.info(f"search:{reg.fullmatch(expr,str(item))}")      
+        logger.info(f"search:{reg.fullmatch(expr,item)}")      
         return reg.fullmatch(expr,str(item)) is not None
     except Exception as e:
         print(f'ERROR: {e}')
