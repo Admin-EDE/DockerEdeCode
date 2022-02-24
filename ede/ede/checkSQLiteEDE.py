a1e137a95bcf7ad1ab694295db7307aa1e5945a5
Admin-EDE <admin@ede.mineduc.cl>
2022-02-24 10:40:30 -0300

    fix fn5E5 change SQL query

--- ede/ede/checkSQLiteEDE.py
+++ ede/ede/checkSQLiteEDE.py
@@ -4403,7 +4403,7 @@ GROUP BY p.personId
           else:
               _result.append(True)
 
-          if _estPresentesCurso[idx_] != (_estPresentesAsign[idx_]+_estAtradadosAsign[idx_]):
+          if _estPresentesCurso[idx_] != (int(_estPresentesAsign[idx_])+int(_estAtradadosAsign[idx_])):
               logger.error(f'Rechazado')
               logger.error(f'Total de estudiantes presentes no coinciden')
               _result.append(False)
