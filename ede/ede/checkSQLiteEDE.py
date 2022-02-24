8e5dd514d4f5f15081789b9a47af3fb73ea39fcc
Admin-EDE <admin@ede.mineduc.cl>
2022-02-24 14:16:20 -0300

    fix fn5E5 change SQL query

--- ede/ede/checkSQLiteEDE.py
+++ ede/ede/checkSQLiteEDE.py
@@ -205,6 +205,8 @@ class check:
     engine = create_engine(f"sqlite+pysqlcipher://:{sec}@/{path}?cipher=aes-256-cfb&kdf_iter=64000")
     try:
       conn = engine.connect()
+      rows = conn.execute("PRAGMA CACHE_SIZE=-100000").fetchone()
+      
 
       for key,value in self.functions.items():
         if(value != "No/Verificado"):
