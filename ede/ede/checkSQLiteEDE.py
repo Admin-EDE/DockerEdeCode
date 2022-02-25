bab08e299738da80014bddf913c51bd5f602343f
Admin-EDE <admin@ede.mineduc.cl>
2022-02-25 13:48:04 -0300

    se agrega nuevo parámetro al comando check

--- ede/ede/checkSQLiteEDE.py
+++ ede/ede/checkSQLiteEDE.py
@@ -348,7 +348,7 @@ class check:
             logger.info(f"{value} ejecutandose con restrición de tiempo {self.args.time} segundos...")
           #  try:
             #eval_ = eval(value)
-            logger.info(f"{key},{self.functionsMultiProcess[key]}")
+            logger.info(f"{key},{self.functionsMultiProcess[key].__kwdefaults__}")
             p = multiprocessing.Process(target=getattr(self.functionsMultiProcess,f"{key}",None), name=value, args=(conn,))
             jobs.append(p)
             p.start()
