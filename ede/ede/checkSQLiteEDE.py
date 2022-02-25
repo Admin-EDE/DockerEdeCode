aad49a8be765e4fd91fb3840179afd6d7c049a70
Admin-EDE <admin@ede.mineduc.cl>
2022-02-25 10:56:01 -0300

    se agrega nuevo parámetro al comando check

--- ede/ede/checkSQLiteEDE.py
+++ ede/ede/checkSQLiteEDE.py
@@ -203,6 +203,7 @@ class check:
     _result = False
     try:
       if(self.time):
+        logger.info(f"Ejecutanto {fn} con restrición de tiempo {self.time} segundos...")
         _result = eval(fn)
       else:
         _result = eval(fn)      
