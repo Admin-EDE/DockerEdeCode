5b663531fffc22571809919a9318a250420bebaf
Admin-EDE <admin@ede.mineduc.cl>
2022-03-01 12:40:24 -0300

    fix fn7F4 se corrige función para ejecutar procesos en paralelo

--- ede/ede/checkSQLiteEDE.py
+++ ede/ede/checkSQLiteEDE.py
@@ -3893,64 +3893,81 @@ GROUP BY p.personId
             - A
           En todo otro caso, retorna False y "Rechazado" a través de logger.
           ]
-    """      
+    """ 
+    _r = False
+    _query = []
     try:
         _query = conn.execute("""
         SELECT LearnerActivityId
         FROM LearnerActivity
         WHERE digitalRandomKey IS NOT NULL
         """).fetchall()
-        if(len(_query)>0):
-            _digitalRandom = conn.execute("""
-            SELECT digitalRandomKey,
-                  DateDigitalRandomKey,
-                  personIDDigitalRandomKey
-            FROM LearnerActivity
-            WHERE LearnerActivityId IN (SELECT LearnerActivityId
-                                        FROM LearnerActivity
-                                        WHERE digitalRandomKey IS NOT NULL)
-            AND DateDigitalRandomKey IS NOT NULL
-            AND personIDDigitalRandomKey IS NOT NULL
-            """).fetchall()
-            if(len(_digitalRandom)==len(_query)):
-              _digitalRandomKeyPerson = conn.execute("""
-              SELECT personIDDigitalRandomKey
-              FROM LearnerActivity
-              WHERE LearnerActivityId IN (SELECT LearnerActivityId
-                                          FROM LearnerActivity
-                                          WHERE digitalRandomKey IS NOT NULL)
-                AND DateDigitalRandomKey IS NOT NULL
-                AND personIDDigitalRandomKey IS NOT NULL
-                AND personIDDigitalRandomKey IN (SELECT P.PersonId
-                                                FROM OrganizationPersonRole OPR
-                                                          JOIN Person P ON OPR.PersonId = P.PersonId
-                                                WHERE OPR.RoleId IN (2, 4, 5));
-              """).fetchall()
-              if(len(_digitalRandom) == len(_digitalRandomKeyPerson)):
-                logger.info(f'Las modificaciones a las ponderaciones cuentan con firma del Docente/UTP')
-                logger.info(f'Aprobado')
-                return_dict[getframeinfo(currentframe()).function] = True
-                return True
-              else:
-                logger.error(f'Las firmas ingresadas no corresponden a las del Docente/UTP')
-                logger.error(f'Rechazado')
-                return_dict[getframeinfo(currentframe()).function] = False
-                return False
-            else:
-              logger.error(f'Se han ingresado datos incompletos para las modificaciones de ponderaciones')
-              logger.error(f'Rechazado')
-              return_dict[getframeinfo(currentframe()).function] = False
-              return False
-        else:
-          logger.error(f'No existen cambios realizados a las ponderaciones ')
-          logger.error(f'S/Datos')
-          return_dict[getframeinfo(currentframe()).function] = False
-          return False
+    except Exception as e:
+      logger.info(f"Resultado: {_query} -> {str(e)}")
+    
+    if( len(_query) == 0 ):
+      logger.error(f'No existen cambios realizados a las ponderaciones ')
+      logger.error(f'S/Datos')
+      return_dict[getframeinfo(currentframe()).function] = _r
+      logger.info(f"{current_process().name} finalizando...")
+      return _r
+
+    _digitalRandom = []
+    try:
+        _digitalRandom = conn.execute("""
+        SELECT digitalRandomKey,
+              DateDigitalRandomKey,
+              personIDDigitalRandomKey
+        FROM LearnerActivity
+        WHERE LearnerActivityId IN (SELECT LearnerActivityId
+                                    FROM LearnerActivity
+                                    WHERE digitalRandomKey IS NOT NULL)
+        AND DateDigitalRandomKey IS NOT NULL
+        AND personIDDigitalRandomKey IS NOT NULL
+        """).fetchall()
+    except Exception as e:
+      logger.info(f"Resultado: {_digitalRandom} -> {str(e)}")
+          
+    if(len(_digitalRandom) != len(_query)):
+      logger.error(f'Se han ingresado datos incompletos para las modificaciones de ponderaciones')
+      logger.error(f'Rechazado')
+      return_dict[getframeinfo(currentframe()).function] = _r
+      logger.info(f"{current_process().name} finalizando...")
+      return _r
+         
+    _digitalRandomKeyPerson = []
+    try:
+      _digitalRandomKeyPerson = conn.execute("""
+      SELECT personIDDigitalRandomKey
+      FROM LearnerActivity
+      WHERE LearnerActivityId IN (SELECT LearnerActivityId
+                                  FROM LearnerActivity
+                                  WHERE digitalRandomKey IS NOT NULL)
+        AND DateDigitalRandomKey IS NOT NULL
+        AND personIDDigitalRandomKey IS NOT NULL
+        AND personIDDigitalRandomKey IN (SELECT P.PersonId
+                                        FROM OrganizationPersonRole OPR
+                                                  JOIN Person P ON OPR.PersonId = P.PersonId
+                                        WHERE OPR.RoleId IN (2, 4, 5));
+      """).fetchall()
+    except Exception as e:
+      logger.info(f"Resultado: {_digitalRandomKeyPerson} -> {str(e)}")
+     
+    try:
+      if(len(_digitalRandom) == len(_digitalRandomKeyPerson)):
+        logger.info(f'Las modificaciones a las ponderaciones cuentan con firma del Docente/UTP')
+        logger.info(f'Aprobado')
+        _r = True
+      else:
+        logger.error(f'Las firmas ingresadas no corresponden a las del Docente/UTP')
+        logger.error(f'Rechazado')
     except Exception as e:
         logger.error(f"No se pudo ejecutar la consulta: {str(e)}")
         logger.error(f"Rechazado")
-        return_dict[getframeinfo(currentframe()).function] = False
-        return False
+    finally:
+      return_dict[getframeinfo(currentframe()).function] = _r
+      logger.info(f"{current_process().name} finalizando...")
+      return _r
   ## Fin fn7F4 WC ##
 
   ## Inicio fn7F5 WC ##
