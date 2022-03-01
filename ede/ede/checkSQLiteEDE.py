d64c37929fb91820a26a844e23560462a50c7649
Admin-EDE <admin@ede.mineduc.cl>
2022-03-01 12:15:15 -0300

    fix fn7F0 se corrige función para ejecutar procesos en paralelo

--- ede/ede/checkSQLiteEDE.py
+++ ede/ede/checkSQLiteEDE.py
@@ -3598,49 +3598,52 @@ GROUP BY p.personId
           En todo otro caso, retorna False y "Rechazado" a través de logger.
           ]
     """      
+    _r = False
+    _query = []
     try:
-        _query = conn.execute("""
-        SELECT A.AssessmentId,
-              ASSR.PersonId,
-              A.RefAssessmentTypeId
-          FROM AssessmentResult R
-                JOIN AssessmentRegistration AR ON AR.AssessmentRegistrationId = R.AssessmentRegistrationId
-                JOIN AssessmentAdministration AA ON AR.AssessmentAdministrationId = AA.AssessmentAdministrationId
-                JOIN Assessment A ON AA.AssessmentId = A.AssessmentId
-                JOIN AssessmentSession ASN ON ASN.AssessmentAdministrationId = AA.AssessmentAdministrationId
-                JOIN AssessmentSessionStaffRole ASSR ON ASN.AssessmentSessionId = ASSR.AssessmentSessionId
-                JOIN OrganizationPersonRole OPR ON OPR.PersonId = ASSR.PersonId
-                  WHERE ASSR.RefAssessmentSessionStaffRoleTypeId = 6
-                    AND OPR.RoleId = 6
-              GROUP BY ASN.AssessmentAdministrationId, ASN.AssessmentSessionId, ASSR.AssessmentSessionStaffRoleId;
-        """).fetchall()
-        if(len(_query)>0):
-            _contador = 0
-            _assessment = int(len(_query))
-            _assessmentType = (list([m[2] for m in _query if m[2] is not None]))
-            for x in _assessmentType:
-                if (x == 28 or x == 29):
-                    _contador += 1
-            if _contador == _assessment:
-                logger.info(f'Todas las evaluaciones estan ingresadas como sumativas o formativas')
-                logger.info(f'Aprobado')
-                return_dict[getframeinfo(currentframe()).function] = True
-                return True
-            else:
-                logger.error(f'No todas las evaluaciones estan ingresadas como sumativas o formativas')
-                logger.error(f'Rechazado')
-                return_dict[getframeinfo(currentframe()).function] = False
-                return False
-        else:
-            logger.error(f'S/Datos')
-            logger.error(f'No se encuentran evaluaciones registradas en el establecimiento')
-            return_dict[getframeinfo(currentframe()).function] = False
-            return False
+      _query = conn.execute("""
+      SELECT A.AssessmentId,
+            ASSR.PersonId,
+            A.RefAssessmentTypeId
+        FROM AssessmentResult R
+              JOIN AssessmentRegistration AR ON AR.AssessmentRegistrationId = R.AssessmentRegistrationId
+              JOIN AssessmentAdministration AA ON AR.AssessmentAdministrationId = AA.AssessmentAdministrationId
+              JOIN Assessment A ON AA.AssessmentId = A.AssessmentId
+              JOIN AssessmentSession ASN ON ASN.AssessmentAdministrationId = AA.AssessmentAdministrationId
+              JOIN AssessmentSessionStaffRole ASSR ON ASN.AssessmentSessionId = ASSR.AssessmentSessionId
+              JOIN OrganizationPersonRole OPR ON OPR.PersonId = ASSR.PersonId
+                WHERE ASSR.RefAssessmentSessionStaffRoleTypeId = 6
+                  AND OPR.RoleId = 6
+            GROUP BY ASN.AssessmentAdministrationId, ASN.AssessmentSessionId, ASSR.AssessmentSessionStaffRoleId;
+      """).fetchall()
     except Exception as e:
-        logger.error(f"No se pudo ejecutar la consulta: {str(e)}")
-        logger.error(f"Rechazado")
-        return_dict[getframeinfo(currentframe()).function] = False
-        return False
+      logger.info(f"Resultado: {_query} -> {str(e)}")
+      
+    try:
+        if(len(_query)>0):
+          _contador = 0
+          _assessment = int(len(_query))
+          _assessmentType = (list([m[2] for m in _query if m[2] is not None]))
+          for x in _assessmentType:
+            if (x == 28 or x == 29):
+              _contador += 1
+          if _contador == _assessment:
+            logger.info(f'Todas las evaluaciones estan ingresadas como sumativas o formativas')
+            logger.info(f'Aprobado')
+            _r = True
+          else:
+            logger.error(f'No todas las evaluaciones estan ingresadas como sumativas o formativas')
+            logger.error(f'Rechazado')
+        else:
+          logger.error(f'S/Datos')
+          logger.error(f'No se encuentran evaluaciones registradas en el establecimiento')
+    except Exception as e:
+      logger.error(f"No se pudo ejecutar la consulta: {str(e)}")
+      logger.error(f"Rechazado")
+    finally:
+      return_dict[getframeinfo(currentframe()).function] = _r
+      logger.info(f"{current_process().name} finalizando...")
+      return _r
   ## Fin fn7F0 WC ##
 
   ## Inicio fn7F1 WC ##
