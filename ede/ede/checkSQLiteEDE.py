06d04b4bd79fe0b8ffa78bdc062db3279dafe4c5
Admin-EDE <admin@ede.mineduc.cl>
2022-04-01 14:04:56 -0300

    fix fn6F0 se arregla condición de S/Datos

--- ede/ede/checkSQLiteEDE.py
+++ ede/ede/checkSQLiteEDE.py
@@ -8029,7 +8029,7 @@ WHERE
           run=str(q1[8])
           fecha_inicio_Estudiante=str(q1[9])
           fecha_fin_Estudiante= fechaActual if(fechaActual <= str(q1[10]) and q1[10] != '1900-01-01') else str(q1[10])
-          diastotal3= int(np.busday_count(fecha_inicio_Estudiante,fecha_fin_Estudiante))
+          diastotal3= int(np.busday_count(fecha_inicio_Estudiante,fecha_fin_Estudiante)) if (fecha_inicio_Estudiante < fecha_fin_Estudiante ) else 0
           logger.debug(f"diastotal3: {diastotal3}")          
           if diastotal3 < (contador2 + contador3):
             diastotal3 = (contador2 + contador3) - diastotal3
