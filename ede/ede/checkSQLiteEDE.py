5f73a39a7b96f418f30e3469077bbbea348e3780
Miguel Aedo Pino <maedo.pino@gmail.com>
2022-04-14 11:40:15 -0400

    fix fn2CB se corrige completamente la función

--- ede/ede/checkSQLiteEDE.py
+++ ede/ede/checkSQLiteEDE.py
@@ -4392,51 +4392,121 @@ WHERE RoleName IN ('Director(a)','Jefe(a) UTP','Inspector(a)','Profesor(a) Jefe'
             - A
           En todo otro caso, retorna False y "Rechazado" a través de logger.
           ]
-    """      
+    """ 
+    _query = []
+    _r = False
     try:
         _query = conn.execute("""
-        SELECT OPR.OrganizationPersonRoleId
-        FROM OrganizationPersonRole OPR
-                JOIN Person P on OPR.PersonId = P.PersonId
-                JOIN PersonStatus PS on P.PersonId = PS.PersonId
-        WHERE OPR.RoleId = 6
-          AND PS.RefPersonStatusTypeId = 30;
+SELECT DISTINCT
+	 pst.personId as 'estudianteId'
+	, pid.Identifier as 'RUN'
+	, oprAlumno.RoleId
+	, prshApoderado.personId as 'ApoderadoId'
+	, prshApoderado.RefPersonRelationshipId
+	, oprApoderado.RoleId
+	, incP.IncidentId
+	, incP.RefIncidentPersonTypeId
+	, incP.Date	
+	, incP.digitalRandomKey
+	, incP.fileScanBase64
+	, doc.fileScanBase64 
+
+FROM PersonStatus pst
+
+-- RESCATA INFORMACIÓN DEL ESTUDIANTE
+OUTER LEFT JOIN PersonIdentifier pid
+	ON pst.personId = pid.personId
+	AND pid.RefPersonIdentificationSystemId IN (SELECT RefPersonIdentificationSystemId FROM RefPersonIdentificationSystem WHERE CODE IN ('RUN'))
+OUTER LEFT JOIN OrganizationPersonRole oprAlumno
+	ON pst.personId = oprAlumno.personId
+	AND oprAlumno.RoleId IN (SELECT RoleId FROM Role WHERE Name IN ('Estudiante'))
+
+-- RESCATA INFORMACIÓN DEL APODERADO O TUTOR ENCARGADO DEL ESTUDIANTE
+OUTER LEFT JOIN PersonRelationship prshApoderado
+	ON prshApoderado.RelatedPersonId = pst.personId
+	AND prshApoderado.RefPersonRelationshipId IN (SELECT RefPersonRelationshipId FROM RefPersonRelationship WHERE Code IN ('Apoderado(a)/Tutor(a)'))
+
+OUTER LEFT JOIN OrganizationPersonRole oprApoderado
+	ON oprApoderado.personId = prshApoderado.personId
+	AND oprApoderado.RoleId IN (SELECT RoleId FROM Role WHERE Name IN ('Padre, madre o apoderado'))
+	
+-- RESCATA INFORMACIÓN RELACIONADA CON LA ENTREGA DE INFORMACIÓN
+OUTER LEFT JOIN IncidentPerson incP
+	ON incP.personId = prshApoderado.personId
+	AND incP.RefIncidentPersonTypeId IN (SELECT RefIncidentPersonTypeId FROM RefIncidentPersonType WHERE Description IN ('Apoderado','Parent/guardian'))
+ 
+OUTER LEFT JOIN Document doc
+	ON incP.fileScanBase64 = documentId
+
+OUTER LEFT JOIN Incident Inc
+	ON inc.IncidentId = incP.IncidentId
+	AND inc.RefIncidentBehaviorId IN (SELECT RefIncidentBehaviorId FROM RefIncidentBehavior WHERE Description IN ('Entrega de documentos retiro de un estudiante') )
+	
+WHERE 
+	pst.RefPersonStatusTypeId IN (SELECT RefPersonStatusTypeId FROM RefPersonStatusType WHERE Description IN ('Estudiante retirado definitivamente')) 
         """).fetchall()
-        if(len(_query)>0):
-            _queryEntregaDocumentos = conn.execute("""
-            SELECT I.IncidentId
-            from Incident I
-                    join IncidentPerson IP on I.IncidentId = IP.IncidentId
-            where I.RefIncidentBehaviorId = 33
-              and I.OrganizationPersonRoleId in (SELECT OPR.OrganizationPersonRoleId
-                                                from OrganizationPersonRole OPR
-                                                          join Person P on OPR.PersonId = P.PersonId
-                                                          join PersonStatus PS on P.PersonId = PS.PersonId
-                                                where 
-                                                  OPR.RoleId = 6
-                                                  and PS.RefPersonStatusTypeId = 30)
-                                                GROUP BY I.IncidentId;
-            """).fetchall()
-            if(len(_query)==len(_queryEntregaDocumentos)):
-              logger.info(f'Todos los alumnos retirados del establecimiento cuentan con una entrega de documentos respectiva al apoderado')
-              logger.info(f'Aprobado')
-              return_dict[getframeinfo(currentframe()).function] = True
-              return True
-            else:
-              logger.error(f'Los alumnos retirados del establecimiento no cuentan con un registro de entrega de documentos al apoderado')
-              logger.error(f'Rechazado')
-              return_dict[getframeinfo(currentframe()).function] = False
-              return False
-        else:
-            logger.error(f'S/Datos')
-            logger.error(f'No existen registro de alumnos retirados del establecimiento')
-            return_dict[getframeinfo(currentframe()).function] = True
-            return True
     except Exception as e:
-        logger.error(f"No se pudo ejecutar la consulta: {str(e)}")
-        logger.error(f"Rechazado")
-        return_dict[getframeinfo(currentframe()).function] = False
-        return False
+      logger.error(f"Resultado: {str(e)}")
+
+    _erroresDetalle = []
+    try:
+      if(not _query):
+        logger.error(f"S/Datos")
+        _r = True
+        raise Exception(f"Sin informacion para verificar")
+      
+      for row in _query:
+        estudianteId = row[0]
+        estudianteRUN = row[1]
+        estudianteRole = row[2]
+        apoderadoId = row[3]
+        apoderadoRefPersonRelationShip = row[4]
+        apoderadoRole = row[5]
+        incidentId = row[6]
+        incidentType = row[7]
+        incidentDate = row[8]
+        incidentKey = row[9]
+        incidentFile = row[10]
+        fileScanBase64 = row[11]
+
+        if(not (estudianteId
+           and
+           estudianteRUN
+           and
+           estudianteRole
+           and
+           apoderadoId
+           and
+           apoderadoId
+           and
+           apoderadoRefPersonRelationShip
+           and 
+           apoderadoRole
+           and
+           incidentId
+           and 
+           incidentType
+           and
+           incidentDate
+           and 
+           (incidentKey or incidentFile)
+           and
+           fileScanBase64)
+           ):
+          _erroresDetalle.append(estudianteId)
+      
+      if(len(_erroresDetalle) > 0):
+        logger.error(f"A los siguientes estudiantes no se les entregó correctamente sus documentos de retiro: {_erroresDetalle}")
+      else:
+        _r = True
+    except Exception as e:
+      logger.error(f"Error on line {sys.exc_info()[-1].tb_lineno}, {type(e).__name__},{e}")
+      logger.error(f"{str(e)}")
+    finally:
+      logger.info(f"Aprobado") if _r else logger.error(f"Rechazado")
+      return_dict[getframeinfo(currentframe()).function] = _r
+      logger.info(f"{current_process().name} finalizando...")      
+      return _r
   ## Fin fn2CB WC ##
 
    ## Inicio fn2BA WC ##
@@ -7844,7 +7914,7 @@ OUTER LEFT JOIN Incident Inc
 	AND inc.RefIncidentBehaviorId IN (SELECT RefIncidentBehaviorId FROM RefIncidentBehavior WHERE Description IN ('Entrega de información para continuidad de estudios') )
 	
 WHERE 
-	pst.RefPersonStatusTypeId IN (SELECT RefPersonStatusTypeId FROM RefPersonStatusType WHERE Description IN ('Estudiante retirado definitivamente'))                          
+	pst.RefPersonStatusTypeId IN (SELECT RefPersonStatusTypeId FROM RefPersonStatusType WHERE Description IN ('Estudiante retirado definitivamente')) 
       """).fetchall()
     except Exception as e:
       logger.info(f"Resultado: {rows} -> {str(e)}")
