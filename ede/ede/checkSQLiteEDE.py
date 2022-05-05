ce86914c03f6502e0499957ec7ff510a3ba4480b
Danilo Olivares Olmos <danilo.olivares@educamos.com>
2022-05-05 11:59:52 +0200

    Arreglo like en las clusulas WHERE

--- ede/ede/checkSQLiteEDE.py
+++ ede/ede/checkSQLiteEDE.py
@@ -5970,8 +5970,8 @@ ON oce.org = Organizationid
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------
 WHERE 
   CAST(strftime('%w',date) as INTEGER) between 1 and 5
-  AND date NOT LIKE "%" || ifnull(oce.fechasEventos,'1900-01-01') || "%"	 
-  AND date NOT LIKE "%" || ifnull(occ.fechasCrisis,'1900-01-01') || "%"
+  AND ifnull(oce.fechasEventos,'1900-01-01') NOT LIKE "%" || date || "%"
+  AND ifnull(occ.fechasCrisis,'1900-01-01') NOT LIKE "%" || date || "%"
   --AND result.idAsignatura NOT NULl
 GROUP BY Organizationid, date
 """).fetchall()
@@ -8284,8 +8284,8 @@ ON oce.org = Organizationid
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------
 WHERE 
   CAST(strftime('%w',date) as INTEGER) between 1 and 5
-  AND date NOT LIKE "%" || ifnull(oce.fechasEventos,'1900-01-01') || "%"	 
-  AND date NOT LIKE "%" || ifnull(occ.fechasCrisis,'1900-01-01') || "%"
+  AND ifnull(oce.fechasEventos,'1900-01-01') NOT LIKE "%" || date || "%"
+  AND ifnull(occ.fechasCrisis,'1900-01-01') NOT LIKE "%" || date || "%"
 GROUP BY Organizationid, date
       """).fetchall()
     except Exception as e:
