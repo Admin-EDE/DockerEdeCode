f26e0b6ce0aa4a4c374dcaddefc5bddc6c7b51fd
Erick Merino <erickfmm@gmail.com>
2022-07-15 10:42:03 -0400

    cambiar nombres de paquetes a nombres oficiales

--- ede/ede/validation_functions_facade.py
+++ ede/ede/validation_functions_facade.py
@@ -1,129 +1,129 @@
 
 #print("Y-Y"*105)
 #registro de salidas o retiros
-from ede.ede.validation_functions.registro_salidas_retiros.fn0FA import fn0FA
-from ede.ede.validation_functions.registro_salidas_retiros.fn0FB import fn0FB
+from ede.ede.validation_functions.fn0_registro_salidas_retiros.fn0FA import fn0FA
+from ede.ede.validation_functions.fn0_registro_salidas_retiros.fn0FB import fn0FB
 
 
 #registro de la entrega de información
-from ede.ede.validation_functions.registro_entrega_informacion.fn1FA import fn1FA
-from ede.ede.validation_functions.registro_entrega_informacion.fn1FB import fn1FB
-from ede.ede.validation_functions.registro_entrega_informacion.fn1FC import fn1FC
+from ede.ede.validation_functions.fn1_registro_entrega_informacion.fn1FA import fn1FA
+from ede.ede.validation_functions.fn1_registro_entrega_informacion.fn1FB import fn1FB
+from ede.ede.validation_functions.fn1_registro_entrega_informacion.fn1FC import fn1FC
 
 
 #registro de matrícula
 
-from ede.ede.validation_functions.registro_matricula.fn28A import fn28A
-from ede.ede.validation_functions.registro_matricula.fn28B import fn28B
-from ede.ede.validation_functions.registro_matricula.fn29A import fn29A
-from ede.ede.validation_functions.registro_matricula.fn29B import fn29B
-from ede.ede.validation_functions.registro_matricula.fn29C import fn29C
-from ede.ede.validation_functions.registro_matricula.fn2AA import fn2AA
-from ede.ede.validation_functions.registro_matricula.fn2BA import fn2BA
-from ede.ede.validation_functions.registro_matricula.fn2CA import fn2CA
-from ede.ede.validation_functions.registro_matricula.fn2CB import fn2CB
-from ede.ede.validation_functions.registro_matricula.fn2DA import fn2DA
-from ede.ede.validation_functions.registro_matricula.fn2DB import fn2DB
-from ede.ede.validation_functions.registro_matricula.fn2EA import fn2EA
-from ede.ede.validation_functions.registro_matricula.fn2FA import fn2FA
+from ede.ede.validation_functions.fn2_registro_matricula.fn28A import fn28A
+from ede.ede.validation_functions.fn2_registro_matricula.fn28B import fn28B
+from ede.ede.validation_functions.fn2_registro_matricula.fn29A import fn29A
+from ede.ede.validation_functions.fn2_registro_matricula.fn29B import fn29B
+from ede.ede.validation_functions.fn2_registro_matricula.fn29C import fn29C
+from ede.ede.validation_functions.fn2_registro_matricula.fn2AA import fn2AA
+from ede.ede.validation_functions.fn2_registro_matricula.fn2BA import fn2BA
+from ede.ede.validation_functions.fn2_registro_matricula.fn2CA import fn2CA
+from ede.ede.validation_functions.fn2_registro_matricula.fn2CB import fn2CB
+from ede.ede.validation_functions.fn2_registro_matricula.fn2DA import fn2DA
+from ede.ede.validation_functions.fn2_registro_matricula.fn2DB import fn2DB
+from ede.ede.validation_functions.fn2_registro_matricula.fn2EA import fn2EA
+from ede.ede.validation_functions.fn2_registro_matricula.fn2FA import fn2FA
 
 
 
 
 
 #integridad de datos
-from ede.ede.validation_functions.integridad_datos.fn3F0 import fn3F0
-from ede.ede.validation_functions.integridad_datos.fn3F1 import fn3F1
-from ede.ede.validation_functions.integridad_datos.fn3F2 import fn3F2
-from ede.ede.validation_functions.integridad_datos.fn3F3 import fn3F3
-from ede.ede.validation_functions.integridad_datos.fn3F4 import fn3F4
-from ede.ede.validation_functions.integridad_datos.fn3F5 import fn3F5
-from ede.ede.validation_functions.integridad_datos.fn3F6 import fn3F6
-from ede.ede.validation_functions.integridad_datos.fn3F7 import fn3F7
-from ede.ede.validation_functions.integridad_datos.fn3F8 import fn3F8
-from ede.ede.validation_functions.integridad_datos.fn3F9 import fn3F9
-from ede.ede.validation_functions.integridad_datos.fn3FA import fn3FA 
-from ede.ede.validation_functions.integridad_datos.fn3FB import fn3FB
-from ede.ede.validation_functions.integridad_datos.fn3FC import fn3FC
-from ede.ede.validation_functions.integridad_datos.fn3FD import fn3FD
-from ede.ede.validation_functions.integridad_datos.fn3FE import fn3FE
-from ede.ede.validation_functions.integridad_datos.fn3FF import fn3FF
+from ede.ede.validation_functions.fn3_integridad_datos.fn3F0 import fn3F0
+from ede.ede.validation_functions.fn3_integridad_datos.fn3F1 import fn3F1
+from ede.ede.validation_functions.fn3_integridad_datos.fn3F2 import fn3F2
+from ede.ede.validation_functions.fn3_integridad_datos.fn3F3 import fn3F3
+from ede.ede.validation_functions.fn3_integridad_datos.fn3F4 import fn3F4
+from ede.ede.validation_functions.fn3_integridad_datos.fn3F5 import fn3F5
+from ede.ede.validation_functions.fn3_integridad_datos.fn3F6 import fn3F6
+from ede.ede.validation_functions.fn3_integridad_datos.fn3F7 import fn3F7
+from ede.ede.validation_functions.fn3_integridad_datos.fn3F8 import fn3F8
+from ede.ede.validation_functions.fn3_integridad_datos.fn3F9 import fn3F9
+from ede.ede.validation_functions.fn3_integridad_datos.fn3FA import fn3FA 
+from ede.ede.validation_functions.fn3_integridad_datos.fn3FB import fn3FB
+from ede.ede.validation_functions.fn3_integridad_datos.fn3FC import fn3FC
+from ede.ede.validation_functions.fn3_integridad_datos.fn3FD import fn3FD
+from ede.ede.validation_functions.fn3_integridad_datos.fn3FE import fn3FE
+from ede.ede.validation_functions.fn3_integridad_datos.fn3FF import fn3FF
 
-from ede.ede.validation_functions.integridad_datos.fn3D0 import fn3D0
-from ede.ede.validation_functions.integridad_datos.fn3D1 import fn3D1
-from ede.ede.validation_functions.integridad_datos.fn3D2 import fn3D2
-from ede.ede.validation_functions.integridad_datos.fn3D3 import fn3D3
-from ede.ede.validation_functions.integridad_datos.fn3DA import fn3DA
-from ede.ede.validation_functions.integridad_datos.fn3DD import fn3DD
-from ede.ede.validation_functions.integridad_datos.fn3E0 import fn3E0
-from ede.ede.validation_functions.integridad_datos.fn3E1 import fn3E1
-from ede.ede.validation_functions.integridad_datos.fn3E2 import fn3E2
-from ede.ede.validation_functions.integridad_datos.fn3E3 import fn3E3
-from ede.ede.validation_functions.integridad_datos.fn3E4 import fn3E4
-from ede.ede.validation_functions.integridad_datos.fn3E5 import fn3E5
-from ede.ede.validation_functions.integridad_datos.fn3E6 import fn3E6
-from ede.ede.validation_functions.integridad_datos.fn3E7 import fn3E7
-from ede.ede.validation_functions.integridad_datos.fn3E8 import fn3E8
-from ede.ede.validation_functions.integridad_datos.fn3E9 import fn3E9
-from ede.ede.validation_functions.integridad_datos.fn3EA import fn3EA
-from ede.ede.validation_functions.integridad_datos.fn3EB import fn3EB
-from ede.ede.validation_functions.integridad_datos.fn3EC import fn3EC
-from ede.ede.validation_functions.integridad_datos.fn3ED import fn3ED
-from ede.ede.validation_functions.integridad_datos.fn3EE import fn3EE
-from ede.ede.validation_functions.integridad_datos.fn3EF import fn3EF
+from ede.ede.validation_functions.fn3_integridad_datos.fn3D0 import fn3D0
+from ede.ede.validation_functions.fn3_integridad_datos.fn3D1 import fn3D1
+from ede.ede.validation_functions.fn3_integridad_datos.fn3D2 import fn3D2
+from ede.ede.validation_functions.fn3_integridad_datos.fn3D3 import fn3D3
+from ede.ede.validation_functions.fn3_integridad_datos.fn3DA import fn3DA
+from ede.ede.validation_functions.fn3_integridad_datos.fn3DD import fn3DD
+from ede.ede.validation_functions.fn3_integridad_datos.fn3E0 import fn3E0
+from ede.ede.validation_functions.fn3_integridad_datos.fn3E1 import fn3E1
+from ede.ede.validation_functions.fn3_integridad_datos.fn3E2 import fn3E2
+from ede.ede.validation_functions.fn3_integridad_datos.fn3E3 import fn3E3
+from ede.ede.validation_functions.fn3_integridad_datos.fn3E4 import fn3E4
+from ede.ede.validation_functions.fn3_integridad_datos.fn3E5 import fn3E5
+from ede.ede.validation_functions.fn3_integridad_datos.fn3E6 import fn3E6
+from ede.ede.validation_functions.fn3_integridad_datos.fn3E7 import fn3E7
+from ede.ede.validation_functions.fn3_integridad_datos.fn3E8 import fn3E8
+from ede.ede.validation_functions.fn3_integridad_datos.fn3E9 import fn3E9
+from ede.ede.validation_functions.fn3_integridad_datos.fn3EA import fn3EA
+from ede.ede.validation_functions.fn3_integridad_datos.fn3EB import fn3EB
+from ede.ede.validation_functions.fn3_integridad_datos.fn3EC import fn3EC
+from ede.ede.validation_functions.fn3_integridad_datos.fn3ED import fn3ED
+from ede.ede.validation_functions.fn3_integridad_datos.fn3EE import fn3EE
+from ede.ede.validation_functions.fn3_integridad_datos.fn3EF import fn3EF
 
-from ede.ede.validation_functions.FR_integridad_datos.fn3C3 import fn3C3
-from ede.ede.validation_functions.FR_integridad_datos.fn3C4 import fn3C4
-from ede.ede.validation_functions.FR_integridad_datos.fn3C5 import fn3C5
-from ede.ede.validation_functions.FR_integridad_datos.fn3CA import fn3CA
-from ede.ede.validation_functions.FR_integridad_datos.fn3D9 import fn3D9
+from ede.ede.validation_functions.fn3_integridad_datos.fn3C3 import fn3C3
+from ede.ede.validation_functions.fn3_integridad_datos.fn3C4 import fn3C4
+from ede.ede.validation_functions.fn3_integridad_datos.fn3C5 import fn3C5
+from ede.ede.validation_functions.fn3_integridad_datos.fn3CA import fn3CA
+from ede.ede.validation_functions.fn3_integridad_datos.fn3D9 import fn3D9
 
-from ede.ede.validation_functions.FR_libro_clases_2.fn6C2 import fn6C2
+from ede.ede.validation_functions.fn6_registro_control_mensual.fn6C2 import fn6C2
 
 
 #libro de classes digital 1
-from ede.ede.validation_functions.libro_clases_digital_1.fn4FA import fn4FA
-from ede.ede.validation_functions.libro_clases_digital_1.fn5D0 import fn5D0
-from ede.ede.validation_functions.libro_clases_digital_1.fn5E0 import fn5E0
-from ede.ede.validation_functions.libro_clases_digital_1.fn5E1 import fn5E1
-from ede.ede.validation_functions.libro_clases_digital_1.fn5E2 import fn5E2
-from ede.ede.validation_functions.libro_clases_digital_1.fn5E3 import fn5E3
-from ede.ede.validation_functions.libro_clases_digital_1.fn5E4 import fn5E4
-from ede.ede.validation_functions.libro_clases_digital_1.fn5E5 import fn5E5
-from ede.ede.validation_functions.libro_clases_digital_1.fn5F0 import fn5F0
+from ede.ede.validation_functions.fn4_registro_antecedentes_generales_alumnos_curso.fn4FA import fn4FA
+from ede.ede.validation_functions.fn5_registro_control_asignaturas.fn5D0 import fn5D0
+from ede.ede.validation_functions.fn5_registro_control_asignaturas.fn5E0 import fn5E0
+from ede.ede.validation_functions.fn5_registro_control_asignaturas.fn5E1 import fn5E1
+from ede.ede.validation_functions.fn5_registro_control_asignaturas.fn5E2 import fn5E2
+from ede.ede.validation_functions.fn5_registro_control_asignaturas.fn5E3 import fn5E3
+from ede.ede.validation_functions.fn5_registro_control_asignaturas.fn5E4 import fn5E4
+from ede.ede.validation_functions.fn5_registro_control_asignaturas.fn5E5 import fn5E5
+from ede.ede.validation_functions.fn5_registro_control_asignaturas.fn5F0 import fn5F0
 
 #libro de classes digital 2
-from ede.ede.validation_functions.libro_clases_digital_2.fn6B0 import fn6B0
-from ede.ede.validation_functions.libro_clases_digital_2.fn680 import fn680
-from ede.ede.validation_functions.libro_clases_digital_2.fn681 import fn681
-from ede.ede.validation_functions.libro_clases_digital_2.fn682 import fn682
-from ede.ede.validation_functions.libro_clases_digital_2.fn6D0 import fn6D0
-from ede.ede.validation_functions.libro_clases_digital_2.fn6C0 import fn6C0
-from ede.ede.validation_functions.libro_clases_digital_2.fn6D1 import fn6D1
-from ede.ede.validation_functions.libro_clases_digital_2.fn6E0 import fn6E0
-from ede.ede.validation_functions.libro_clases_digital_2.fn6E1 import fn6E1
-from ede.ede.validation_functions.libro_clases_digital_2.fn6E2 import fn6E2
-from ede.ede.validation_functions.libro_clases_digital_2.fn6E3 import fn6E3
-from ede.ede.validation_functions.libro_clases_digital_2.fn6E4 import fn6E4
-from ede.ede.validation_functions.libro_clases_digital_2.fn6F0 import fn6F0
-from ede.ede.validation_functions.libro_clases_digital_2.fn6F1 import fn6F1
+from ede.ede.validation_functions.fn6_registro_control_mensual.fn6B0 import fn6B0
+from ede.ede.validation_functions.fn6_registro_control_mensual.fn680 import fn680
+from ede.ede.validation_functions.fn6_registro_control_mensual.fn681 import fn681
+from ede.ede.validation_functions.fn6_registro_control_mensual.fn682 import fn682
+from ede.ede.validation_functions.fn6_registro_control_mensual.fn6D0 import fn6D0
+from ede.ede.validation_functions.fn6_registro_control_mensual.fn6C0 import fn6C0
+from ede.ede.validation_functions.fn6_registro_control_mensual.fn6D1 import fn6D1
+from ede.ede.validation_functions.fn6_registro_control_mensual.fn6E0 import fn6E0
+from ede.ede.validation_functions.fn6_registro_control_mensual.fn6E1 import fn6E1
+from ede.ede.validation_functions.fn6_registro_control_mensual.fn6E2 import fn6E2
+from ede.ede.validation_functions.fn6_registro_control_mensual.fn6E3 import fn6E3
+from ede.ede.validation_functions.fn6_registro_control_mensual.fn6E4 import fn6E4
+from ede.ede.validation_functions.fn6_registro_control_mensual.fn6F0 import fn6F0
+from ede.ede.validation_functions.fn6_registro_control_mensual.fn6F1 import fn6F1
 
 #libro de classes digital 3
-from ede.ede.validation_functions.libro_clases_digital_3.fn7F5 import fn7F5
-from ede.ede.validation_functions.libro_clases_digital_3.fn7F4 import fn7F4
-from ede.ede.validation_functions.libro_clases_digital_3.fn7F3 import fn7F3
-from ede.ede.validation_functions.libro_clases_digital_3.fn7F2 import fn7F2
-from ede.ede.validation_functions.libro_clases_digital_3.fn7F1 import fn7F1
-from ede.ede.validation_functions.libro_clases_digital_3.fn7F0 import fn7F0
+from ede.ede.validation_functions.fn7_registro_evaluaciones_sectores_educativos.fn7F5 import fn7F5
+from ede.ede.validation_functions.fn7_registro_evaluaciones_sectores_educativos.fn7F4 import fn7F4
+from ede.ede.validation_functions.fn7_registro_evaluaciones_sectores_educativos.fn7F3 import fn7F3
+from ede.ede.validation_functions.fn7_registro_evaluaciones_sectores_educativos.fn7F2 import fn7F2
+from ede.ede.validation_functions.fn7_registro_evaluaciones_sectores_educativos.fn7F1 import fn7F1
+from ede.ede.validation_functions.fn7_registro_evaluaciones_sectores_educativos.fn7F0 import fn7F0
 
 #libro de classes digital 4
-from ede.ede.validation_functions.libro_clases_digital_4.fn8F3 import fn8F3
-from ede.ede.validation_functions.libro_clases_digital_4.fn8F2 import fn8F2
-from ede.ede.validation_functions.libro_clases_digital_4.fn8F1 import fn8F1
-from ede.ede.validation_functions.libro_clases_digital_4.fn8F0 import fn8F0
+from ede.ede.validation_functions.fn8_registro_anotaciones_convivencia_escolar_por_estudiante.fn8F3 import fn8F3
+from ede.ede.validation_functions.fn8_registro_anotaciones_convivencia_escolar_por_estudiante.fn8F2 import fn8F2
+from ede.ede.validation_functions.fn8_registro_anotaciones_convivencia_escolar_por_estudiante.fn8F1 import fn8F1
+from ede.ede.validation_functions.fn8_registro_anotaciones_convivencia_escolar_por_estudiante.fn8F0 import fn8F0
 
 #libro de classes digital 5
-from ede.ede.validation_functions.libro_clases_digital_5.fn9F3 import fn9F3
-from ede.ede.validation_functions.libro_clases_digital_5.fn9F2 import fn9F2
-from ede.ede.validation_functions.libro_clases_digital_5.fn9F1 import fn9F1
-from ede.ede.validation_functions.libro_clases_digital_5.fn9F0 import fn9F0
+from ede.ede.validation_functions.fn9_registro_atencion_profesionales_y_recursos_relacionados_con_la_formacion_del_estudiante.fn9F3 import fn9F3
+from ede.ede.validation_functions.fn9_registro_atencion_profesionales_y_recursos_relacionados_con_la_formacion_del_estudiante.fn9F2 import fn9F2
+from ede.ede.validation_functions.fn9_registro_atencion_profesionales_y_recursos_relacionados_con_la_formacion_del_estudiante.fn9F1 import fn9F1
+from ede.ede.validation_functions.fn9_registro_atencion_profesionales_y_recursos_relacionados_con_la_formacion_del_estudiante.fn9F0 import fn9F0
