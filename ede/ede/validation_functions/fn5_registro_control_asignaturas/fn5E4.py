3ee2750113b7ad31eba584868fee11301aa162ee
Erick Merino Menares <erickfmm@gmail.com>
2022-07-28 15:33:33 -0400

    fix errores de tabulacion, faltan fn28b, 29b, 29c, 2aa, 2ba

--- ede/ede/validation_functions/fn5_registro_control_asignaturas/fn5E4.py
+++ ede/ede/validation_functions/fn5_registro_control_asignaturas/fn5E4.py
@@ -24,39 +24,38 @@ def fn5E4(conn, return_dict):
           ]
     """
     try:
-        _query = conn.execute("""--sql
+        _query = conn.execute("""
         SELECT RAE.DATE,
               RAE.RefAttendanceStatusId
         FROM OrganizationPersonRole OPR
                 join RoleAttendanceEvent RAE on OPR.OrganizationPersonRoleId = RAE.OrganizationPersonRoleId
-        where OPR.RoleId = 6 --Estudiante
+        where OPR.RoleId = 6
         and RAE.Date is not null;
         """).fetchall()
-        if(len(_query) > 0):
-            _date = (list(set([m[0] for m in _query if m[0] is not None])))
-            if not _date:
-                logger.error(f"Sin fecha de asistencia ingresada")
-                logger.error(f'Rechazado')
-                return_dict[getframeinfo(currentframe()).function] = False
-                return False
-            _status = (list(set([m[1] for m in _query if m[1] is not None])))
-            if not _status:
-                logger.error(f"Sin estado de asistencia asignado")
-                logger.error(f'Rechazado')
-                return_dict[getframeinfo(currentframe()).function] = False
-                return False
-            logger.info(f'Aprobado')
-            logger.info(
-                f'Todos los registros de asistencia cuentan con un estado asignado')
-            return_dict[getframeinfo(currentframe()).function] = True
-            return True
+        if(len(_query)>0):
+          _date = (list(set([m[0] for m in _query if m[0] is not None])))
+          if not _date:
+            logger.error(f"Sin fecha de asistencia ingresada")
+            logger.error(f'Rechazado')
+            return_dict[getframeinfo(currentframe()).function] = False
+            return False
+          _status = (list(set([m[1] for m in _query if m[1] is not None])))
+          if not _status:
+            logger.error(f"Sin estado de asistencia asignado")
+            logger.error(f'Rechazado')
+            return_dict[getframeinfo(currentframe()).function] = False
+            return False
+          logger.info(f'Aprobado')
+          logger.info(f'Todos los registros de asistencia cuentan con un estado asignado')
+          return_dict[getframeinfo(currentframe()).function] = True
+          return True
         else:
             logger.info(f"S/Datos")
             logger.info(f"Sin datos de asistencia")
             return_dict[getframeinfo(currentframe()).function] = False
             return False
     except Exception as e:
-        logger.error(f"No se pudo ejecutar la consulta: {str(e)}")
-        logger.error(f"Rechazado")
-        return_dict[getframeinfo(currentframe()).function] = False
-        return False
+          logger.error(f"No se pudo ejecutar la consulta: {str(e)}")
+          logger.error(f"Rechazado")
+          return_dict[getframeinfo(currentframe()).function] = False
+          return False
