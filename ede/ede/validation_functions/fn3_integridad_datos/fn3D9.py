849907e9d07c0812f801ce97b84b958a6539be14
admin-ede <admin@ede.mineduc.cl>
2022-10-14 15:44:50 -0300

    optimizar fn3D9

--- ede/ede/validation_functions/fn3_integridad_datos/fn3D9.py
+++ ede/ede/validation_functions/fn3_integridad_datos/fn3D9.py
@@ -8,7 +8,7 @@ from ede.ede._logger import logger
 def fn3D9(conn, return_dict):
     """
     INTEGRIDAD DE DATOS
-    
+
     Las asignaturas tienen sus sesiones de calendario (cuándo será la clase) y sus asistencias.
     -----
     (Tabla RoleAttendance)
@@ -65,69 +65,51 @@ def fn3D9(conn, return_dict):
 
     RoleAttendance = []
     try:
-        RoleAttendance = ejecutar_sql(conn, """--sql
+        RoleAttendance_outer = ejecutar_sql(conn, """--sql
         /*
         * verifica que los registro de calendar Session y RoleAttendanceEvent sean consistentes.
         */
-        SELECT r.OrganizationId, r.RoleAttendanceEventid, OrganizationCalendarSession.OrganizationCalendarSessionId
-        FROM (
-          SELECT
-            Organization.OrganizationId
-            , RoleAttendanceEvent.RoleAttendanceEventid
-            , Organization.RefOrganizationTypeId
-            --, AttendanceTermIndicator
-            , OrganizationCalendarSession.OrganizationCalendarSessionId
-            , DATETIME(DATE(BeginDate) || 'T' || TIME(SessionStartTime)) as 'InicioClase'
-            --, RoleAttendanceEvent.Date
-            --, DATETIME(DATE(EndDate) || 'T' || TIME(SessionEndTime)) as 'FinClase'
-            --, *
-          FROM Organization
-          OUTER LEFT JOIN RefOrganizationType
-           ON Organization.RefOrganizationTypeId = RefOrganizationType.RefOrganizationTypeId
-           AND Organization.RefOrganizationTypeId = 22 --.Description = 'Course Section'
-          OUTER LEFT JOIN OrganizationCalendar
-           ON Organization.OrganizationId = OrganizationCalendar.OrganizationId
-          OUTER LEFT JOIN OrganizationCalendarSession
-           ON OrganizationCalendar.OrganizationCalendarId = OrganizationCalendarSession.OrganizationCalendarId
-           AND OrganizationCalendarSession.AttendanceTermIndicator = 1
-          OUTER LEFT JOIN OrganizationPersonRole
-           ON Organization.OrganizationId = OrganizationPersonRole.OrganizationId
-          OUTER LEFT JOIN RoleAttendanceEvent 
-           ON OrganizationPersonRole.OrganizationPersonRoleId = RoleAttendanceEvent.OrganizationPersonRoleId
-          WHERE
-          --RefOrganizationType.Description IN ('Course Section')
-          --AND
-          RoleAttendanceEvent.Date = InicioClase
-          --AND AttendanceTermIndicator = 1
-        ) as r
-        INNER JOIN RefOrganizationType
-         ON r.RefOrganizationTypeId = RefOrganizationType.RefOrganizationTypeId
-        INNER JOIN OrganizationCalendar
-         ON r.OrganizationId = OrganizationCalendar.OrganizationId
-        INNER JOIN OrganizationCalendarSession
+        SELECT Organization.OrganizationId, RoleAttendanceEvent.RoleAttendanceEventid, OrganizationCalendarSession.OrganizationCalendarSessionId
+        FROM Organization
+        OUTER LEFT JOIN RefOrganizationType
+         ON Organization.RefOrganizationTypeId = RefOrganizationType.RefOrganizationTypeId
+         AND RefOrganizationType.RefOrganizationTypeId == 22 -- 'Course Section'
+        OUTER LEFT JOIN OrganizationCalendar
+         ON Organization.OrganizationId = OrganizationCalendar.OrganizationId
+        OUTER LEFT JOIN OrganizationCalendarSession
          ON OrganizationCalendar.OrganizationCalendarId = OrganizationCalendarSession.OrganizationCalendarId
-        INNER JOIN OrganizationPersonRole
-         ON r.OrganizationId = OrganizationPersonRole.OrganizationId
-        INNER JOIN RoleAttendanceEvent
+         AND OrganizationCalendarSession.AttendanceTermIndicator = 1
+        OUTER LEFT JOIN OrganizationPersonRole
+         ON Organization.OrganizationId = OrganizationPersonRole.OrganizationId
+        OUTER LEFT JOIN RoleAttendanceEvent
          ON OrganizationPersonRole.OrganizationPersonRoleId = RoleAttendanceEvent.OrganizationPersonRoleId
+		 WHERE
+		   Organization.RefOrganizationTypeId = 22 --.Description = 'Course Section'
+		   AND OrganizationCalendarSession.AttendanceTermIndicator = 1
+       AND DATE(RoleAttendanceEvent.Date) = OrganizationCalendarSession.BeginDate
+		   AND DATE(RoleAttendanceEvent.Date) = OrganizationCalendarSession.EndDate
       """)
     except Exception as e:
-        logger.info(f"Resultado: {RoleAttendance} -> {str(e)}")
+        logger.info(f"Resultado: {RoleAttendance_outer} -> {str(e)}")
 
-    logger.info(f"Eventos mal identificados: {len(RoleAttendance)}")
+    logger.info(
+        f"Eventos mal identificados: {len(RoleAttendance_outer) - len(listInfoSuccesfull)}")
     try:
-        if(len(RoleAttendance) > 0):
-            data1 = list(
-                set([m[0] for m in RoleAttendance if m[0] is not None]))
-            _c1 = len(set(data1))
-            _err1 = f"Las siguientes organizaciones no coinciden: {data1}"
-            if (_c1 > 0):
-                logger.error(_err1)
-                logger.error(f"Rechazado")
-                _r = False
-            else:
-              logger.info(f"Aprobado")
-              _r = True
+        if(len(RoleAttendance_outer) - len(listInfoSuccesfull) > 0):
+            for x in RoleAttendance_outer:
+                if x not in listInfoSuccesfull:
+                    RoleAttendance.append(x)
+                data1 = list(
+                    set([m[0] for m in RoleAttendance if m[0] is not None]))
+                _c1 = len(set(data1))
+                _err1 = f"Las siguientes organizaciones no coinciden: {data1}"
+                if (_c1 > 0):
+                    logger.error(_err1)
+                    logger.error(f"Rechazado")
+                    _r = False
+                else:
+                    logger.info(f"Aprobado")
+                    _r = True
         else:
             logger.info(f"Aprobado")
             _r = True
